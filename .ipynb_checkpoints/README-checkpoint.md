676cf282c26674c2c745035c6783e34698b6830b
raph ael <r.eissler@gmx.de>
2020-06-16 23:54:28 +0200

    added first plots to the plot notebook + updatet the data functions

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,2 @@
+# covid19mexico
+Code to evaluate the data from the mexican government
--- .ipynb_checkpoints/dataFunctions-checkpoint.py
+++ .ipynb_checkpoints/dataFunctions-checkpoint.py
@@ -4,27 +4,6 @@
 A set of Functions for the covid data
 """
 
-from datetime import date
-import pandas as pd
-from datetime import date
-import os
-
-today = date.today()
-
-
-def getCsvFromDate(dataDir : str, csvDate : date = today):
-    ''' Read a csv from the given data and return is as a pd object '''
-    if type(csvDate) is not date:
-        raise Exception("The given date is no from the type: datetime.date")
-    
-    fileName = os.path.join(dataDir, "{year}{month:02d}{day}COVID19MEXICO.csv".format(year=str(csvDate.year)[2:4], # the last two numbers from the year
-                                                                                 month=csvDate.month,
-                                                                                 day=csvDate.day))
-    if not os.path.isfile(fileName):
-        raise Exception("Data from {} does not exists! Path: {}".format(csvDate, fileName))        
-    
-    return pd.read_csv(fileName, encoding = "ISO-8859-1")
-
 def getPendingCasesDf(df, desc=3, column="RESULTADO"):
     ''' Returns a dataframe of all pending cases'''
     return df[df[column] == desc]
